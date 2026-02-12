cf1893d5418a92ed988f4e7b1d09a0d4011dfc75
Julian Einzinger <julian5einzinger@gmail.com>
2026-02-12 17:08:28 +0100

    Added model type for User & UserRow

---
+++ backend/src/model.ts
@@ -0,0 +1,11 @@
+export type User = {
+    id: string,
+    username: string,
+    passwordHash: string
+}
+
+export type UserRow = {
+    ID: string,
+    USERNAME: string,
+    PASSWORD_HASH: string
+}

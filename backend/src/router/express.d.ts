f3f30abf4be34d9cbc5ba2f1d35eb4e384a439a1
Julian Einzinger <julian5einzinger@gmail.com>
2026-02-13 18:18:35 +0100

    Changed expres-request interface to include TokenPayload

--- backend/src/router/express.d.ts
+++ backend/src/router/express.d.ts
@@ -1,9 +1,10 @@
 import { Request } from "express";
+import type { TokenPayload } from "../services/authService.ts";
 
 declare global {
     namespace Express {
         export interface Request {
-            user?: { username: string };
+            user?: TokenPayload;
         }
     }
 }
